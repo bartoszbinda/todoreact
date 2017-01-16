1ef4a2cc4d74ab9a5201f79b201dfea003a5c59f
Bartosz <batinex@gmail.com>
2017-01-16 14:32:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+= Warning, something with later versions of foundation is broken
+= To Do: update webpack to use newer version of foundation
---
+++ app/tests/app.test.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var TestUtils = require('react-addons-test-utils');
+require('script!jquery/dist/jquery.min.js');
+var $ = require('jquery');
+describe('App', () => {
+  it('should properly run tests', () => {
+    expect(1).toBe(1);
+  });
+});
