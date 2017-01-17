5a7ac5a85a0096b08c41e6123ea87f58cad642dc
Bartosz <batinex@gmail.com>
2017-01-17 23:31:16 +0100

    created test for search, created playground

---
+++ playground/spread.js
@@ -0,0 +1,14 @@
+// function add(a, b) {
+//     return a + b;
+// };
+// console.log(add(3, 1));
+//
+// var toAdd = [9, 5];
+//
+// console.log(add(...toAdd));
+var groupA = ['bobo', 'kotek'];
+var groupB = ['kotko'];
+var final = [
+    3, ...groupA,
+    ...groupB
+];
