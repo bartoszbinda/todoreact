de543e56c7cfe0d241fb890233e9999fcdecc1a2
Bartosz <batinex@gmail.com>
2017-02-08 22:58:17 +0100

    created logout reducer

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -4,30 +4,46 @@ var {connect} = require("react-redux");
 var actions = require("actions");
 
 export var Todo = React.createClass({
-	render: function() {
-		var {text, id, completed, createdAt, completedAt, dispatch} = this.props;
-		var todoClassName = completed ? "todo todo-completed" : "todo";
-		var renderDate = () => {
-			var message = "Created ";
-			var timestamp = createdAt;
-			if (completed) {
-				message = "Completed ";
-				timestamp = completedAt;
-			}
-			return message + moment.unix(timestamp).format("MMM Do YYYY @ h:mm a");
-		};
-		return (
+    render: function() {
+        var {
+            text,
+            id,
+            completed,
+            createdAt,
+            completedAt,
+            dispatch
+        } = this.props;
+        var todoClassName = completed
+            ? "todo todo-completed"
+            : "todo";
+        var renderDate = () => {
+            var message = "Created ";
+            var timestamp = createdAt;
+            if (completed) {
+                message = "Completed ";
+                timestamp = completedAt;
+            }
+            return message + moment
+                .unix(timestamp)
+                .format("MMM Do YYYY @ h:mm a");
+        };
+        return (
             <div className={todoClassName} onClick={() => {
-            	dispatch(actions.startToggleTodo(id,!completed))
-}}>
-            <div> <input type="checkbox" checked={completed}/> </div>
-            <div>
-            <p> {text} </p>
-            <p className="todo__subtext"> {renderDate()}</p>
+                dispatch(actions.startToggleTodo(id, !completed))
+            }}>
+                <div>
+                    <input type="checkbox" checked={completed}/>
+                </div>
+                <div>
+                    <p>
+                        {text}
+                    </p>
+                    <p className="todo__subtext">
+                        {renderDate()}</p>
+                </div>
             </div>
-            </div>
-		);
-	}
+        );
+    }
 });
 
 export default connect()(Todo)
